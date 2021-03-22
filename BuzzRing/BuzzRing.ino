29588bd4ba45dfc6bc8ed522d48a89fabca25ae4
אמיר דגן <amir.dgn@outlook.com>
2021-03-23 00:45:58 +0200

    Add project files.

---
+++ BuzzRing/BuzzRing.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BuzzRing.ino
+ Created:	3/23/2021 12:36:51 AM
+ Author:	amird
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
